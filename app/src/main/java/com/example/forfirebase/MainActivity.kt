96d1af533ae498586bc8ec93924367c75dccca83
SHyeonL <kjghost1@naver.com>
2022-11-13 05:19:03 +0900

    ReUpload

--- app/src/main/java/com/example/forfirebase/MainActivity.kt
+++ app/src/main/java/com/example/forfirebase/MainActivity.kt
@@ -7,17 +7,38 @@ import com.example.forfirebase.databinding.ActivityMainBinding
 import com.google.firebase.database.*
 import com.google.firebase.ktx.Firebase
 
+
 class MainActivity : AppCompatActivity() {
 
-    val database: FirebaseDatabase = FirebaseDatabase.getInstance()
-    val myRef: DatabaseReference = database.getReference("tree")
-    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    var lng = "NULL"
+    var lat = "NULL"
+    val database : FirebaseDatabase = FirebaseDatabase.getInstance()
+    val myRef : DatabaseReference = database.getReference("tree")
+
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater)}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        readOnce()
+        with(binding){
+            btnPost.setOnClickListener {
+                readXY()
+//                val name = editName.text.toString()
+//                val password = editPassword.text.toString()
+//                //val lng = myRef.child("777").get()
+//                Log.d("파이어베이스: POST버튼 누름", lng)
+//                readXY()
+//                Log.d("파이어베이스: readXY() 실행완료", lng)
+//                val user = User(name, password, lng, lat)
+//                Log.d("파이어베이스: user 개체 생성", lng)
+//                addItem(user)
+                Log.d("파이어베이스: addItem", lng)
+            }
+            btnTest.setOnClickListener {
+                readOnce()
+            }
+        }
 
 //        myRef.addValueEventListener( object: ValueEventListener {
 //            override fun onDataChange(snapshot: DataSnapshot) {
@@ -26,7 +47,8 @@ class MainActivity : AppCompatActivity() {
 //                    textList.setText("")
 //                    for (item in snapshot.children) {
 //                        item.getValue(User::class.java)?.let { user ->
-//                            textList.append("${user.name} : ${user.password} \n")
+//                            textList.append("${user.id} / ${user.lat} / ${user.lng}\n")
+//                            textList.append("${user.tree_name} : ${user.user_name} \n")
 //                        }
 //                    }
 //                }
@@ -37,39 +59,78 @@ class MainActivity : AppCompatActivity() {
 //                print(error.message)
 //            }
 //        })
-    }
 
+        // write()
+        readOnce()
+        // readData()
+    }
+    // 목록 형태로 추가하기
+    fun addItem(user:User) {
+        val id = "0"
+        user.id = "0"
+        myRef.child(id).setValue(user)
+    }
+    // 계속 읽기
     fun readData() {
-        myRef.child("name").addValueEventListener(object : ValueEventListener {
+        myRef.child("name").addValueEventListener( object: ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 Log.d("파이어베이스", "${snapshot.value}")
-                print(snapshot.value) // 값이 변경 될때마다 매번 호출
+                print( snapshot.value ) // 값이 변경 될때마다 매번 호출
             }
-
             override fun onCancelled(error: DatabaseError) {
-                print(error.message)
+                print( error.message )
             }
         })
     }
-
+    // 한번 읽기
     fun readOnce() {
-        myRef.get().addOnSuccessListener {
-            Log.d("파이어베이스", "name=${it.value}") // 한 번 호출
+        myRef.child("777").get().addOnSuccessListener {
+            Log.d("파이어베이스", "name=${it.child("lng").value}") // 한 번 호출
         }.addOnFailureListener {
             Log.d("파이어베이스", "error=${it}")
         }
     }
+
+    fun readXY() {
+        myRef.child("0").get().addOnSuccessListener {
+            lng = it.child("lng").value.toString()
+            Log.d("파이어베이스", lng) // 한 번 호출
+            lat = it.child("lat").value.toString()
+            Log.d("파이어베이스", lat) // 한 번 호출
+            val name = binding.editName.text.toString()
+            val password = binding.editPassword.text.toString()
+            //val lng = myRef.child("777").get()
+//            Log.d("파이어베이스: POST버튼 누름", lng)
+//            Log.d("파이어베이스: readXY() 실행완료", lng)
+            val user = User(name, password, lng, lat)
+            Log.d("파이어베이스: user 개체 생성", lng)
+            addItem(user)
+            Log.d("파이어베이스: addItem", lng)
+        }.addOnFailureListener {
+            Log.d("파이어베이스", "error=${it}")
+        }
+    }
+    // 쓰기
+    fun write() {
+        myRef.child("name").setValue("Scott")
+        myRef.child("age").setValue(19)
+    }
 }
+
 
 class User {
     var id:String = ""
-    var name:String = ""
-    var password:String = ""
+    var lat:String = ""
+    var lng:String = ""
+    var tree_name:String = ""
+    var user_name:String = ""
 
     constructor() // 파이어베이스에서 데이터 변환을 위해서 필요
 
-    constructor(name:String, password:String) {
-        this.name = name
-        this.password = password
+    constructor(tree_name:String, user_name:String, lng:String, lat:String) {
+        this.tree_name = tree_name
+        this.user_name = user_name
+        this.lng = lng
+        this.lat = lat
     }
 }
